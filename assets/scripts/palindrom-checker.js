6cab00415080662f7a94d5a8c13d9f56103befb0
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-20 08:00:48 +0100

    feat: create function to check for palindrome

--- assets/scripts/palindrom-checker.js
+++ assets/scripts/palindrom-checker.js
@@ -1,1 +1,24 @@
 'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('check-btn')
+  const display = document.getElementById('display')
+
+  // Function to check if the string is a palindrome
+
+  const palindromeChecker = (string) => {
+
+    /**
+     * filters out non-alphanumeric characters and converts the input string to lowercase before checking for palindromes
+     */
+
+    const filteredString = string.toLowerCase().split('').filter(char => {
+      return (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')
+    })
+
+    const reversedString = filteredString.slice().reverse()
+
+    return filteredString.join('') === reversedString.join('')
+  };
+
+})
